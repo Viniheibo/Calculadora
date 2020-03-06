9bbff1968cbc3148a1ffdaabeec1cac5aec10589
Viniheibo <Viniheibo@users.noreply.github.com>
2020-03-05 21:00:15 -0300

    Alterado saída de resultado para JOption

--- CalculadoraIFC/src/main/java/Calculadora/Calculadora.java
+++ CalculadoraIFC/src/main/java/Calculadora/Calculadora.java
@@ -1,70 +1,86 @@
 package Calculadora;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Calculadora {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        double n1, resultado;
-        double n2 = 0;
+        double n1, n2, resultado;
         String operacao;
 
         System.out.println("Digite a operação desejada (+, -, *, /, ^, raiz): ");
         operacao = entrada.nextLine();
 
-        if ("+".equals(operacao)) {
-            System.out.println("Digite o primeiro valor: ");
-            n1 = entrada.nextDouble();
-            System.out.println("Digite o segundo valor: ");
-            n2 = entrada.nextDouble();
-            resultado = n1 + n2;
-            System.out.println("Resultado: " + resultado);
-        } else if ("-".equals(operacao)) {
-            System.out.println("Digite o primeiro valor: ");
-            n1 = entrada.nextDouble();
-            System.out.println("Digite o segundo valor: ");
-            n2 = entrada.nextDouble();
-            resultado = n1 - n2;
-            System.out.println("Resultado: " + resultado);
-        } else if ("*".equals(operacao)) {
-            System.out.println("Digite o primeiro valor: ");
-            n1 = entrada.nextDouble();
-            System.out.println("Digite o segundo valor: ");
-            n2 = entrada.nextDouble();
-            resultado = n1 * n2;
-            System.out.println("Resultado: " + resultado);
-        } else if ("^".equals(operacao)) {
-            System.out.println("Digite o primeiro valor: ");
-            n1 = entrada.nextDouble();
-            System.out.println("Digite o segundo valor: ");
-            n2 = entrada.nextDouble();
-            resultado = Math.pow(n1, n2);
-            System.out.println("Resultado: " + resultado);
-        } else if ("raiz".equals(operacao)) {
-            while (true) {
-                System.out.println("Digite o valor: ");
-                n1 = entrada.nextDouble();
-                if (n1 >= 0) {
-                    resultado = Math.sqrt(n1);
-                    System.out.println("A raiz quadrada de " + n1 + " é: " + resultado);
+        if (null != operacao) {
+            switch (operacao) {
+                case "+":
+                    System.out.println("primeiro valor");
+                    n1 = entrada.nextDouble();
+                    System.out.println("Digite o segundo valor: ");
+                    n2 = entrada.nextDouble();
+                    resultado = n1 + n2;
+                    JOptionPane.showMessageDialog(null, "Resultado: " + resultado);
                     break;
-                } else {
-                    System.out.println("Raiz não permite número negativo");
-                }
+                case "-":
+                    System.out.println("Digite o primeiro valor: ");
+                    n1 = entrada.nextDouble();
+                    System.out.println("Digite o segundo valor: ");
+                    n2 = entrada.nextDouble();
+                    resultado = n1 - n2;
+                    JOptionPane.showMessageDialog(null, "Resultado: " + resultado);
+                    break;
+                case "*":
+                    System.out.println("Digite o primeiro valor: ");
+                    n1 = entrada.nextDouble();
+                    System.out.println("Digite o segundo valor: ");
+                    n2 = entrada.nextDouble();
+                    resultado = n1 * n2;
+                    JOptionPane.showMessageDialog(null, "Resultado: " + resultado);
+                    break;
+                case "/":
+                    System.out.println("Digite o primeiro valor: ");
+                    n1 = entrada.nextDouble();
+                    System.out.println("Digite o segundo valor: ");
+                    n2 = entrada.nextDouble();
+                    while (n2 == 0) {
+                        if (n2 != 0) {
+                            resultado = n1 / n2;
+                            JOptionPane.showMessageDialog(null, "Resultado: " + resultado);
+                            break;
+                        } else if (n2 == 0) {
+                            System.out.println("Não é possível dividir por zero, favor digitar outro número");
+                            n2 = entrada.nextDouble();
+                        }
+                    }
+                    break;
+                case "^":
+                    System.out.println("Digite o primeiro valor: ");
+                    n1 = entrada.nextDouble();
+                    System.out.println("Digite o segundo valor: ");
+                    n2 = entrada.nextDouble();
+                    resultado = Math.pow(n1, n2);
+                    JOptionPane.showMessageDialog(null, "Resultado: " + resultado);
+                    break;
+                case "raiz":
+                    while (true) {
+                        System.out.println("Digite o valor: ");
+                        n1 = entrada.nextDouble();
+                        if (n1 >= 0) {
+                            resultado = Math.sqrt(n1);
+                            System.out.println("A raiz quadrada de " + n1 + " é: " + resultado);
+                            JOptionPane.showMessageDialog(null, "A raiz quadrada de " + n1 + " é: " + resultado);
+                            break;
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Raiz não permite número negativo");
+                        }
+                    }
+                    break;
+                default:
+                    break;
             }
         }
-        while ("/".equals(operacao) && n2 == 0) {
-            System.out.println("Digite o primeiro valor: ");
-            n1 = entrada.nextDouble();
-            System.out.println("Digite o segundo valor: ");
-            n2 = entrada.nextDouble();
-            System.out.println("Não é possível dividir por zero, favor digitar outro número");
-            n2 = entrada.nextDouble();
-            resultado = n1 / n2;
-            if (n2 != 0) {
-                System.out.println("Resultado: " + resultado);
-            }
-        }
+
     }
 }
